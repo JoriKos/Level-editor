e866a239723e27a45074e3940a3a56132050fe58
DOOT <26204@ma-web.nl>
2021-09-28 23:58:14 +0200

    Grid start

---
+++ Assets/Scripts/GridTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    private void Awake()
+    {
+        Grid grid = new Grid(20, 10, 10f);
+    }
+}
